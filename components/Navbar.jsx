71b71ac893e21be561745830c844d5110d0fdf4a
Daniel Asena <asenadaniel@gmail.com>
2025-09-12 10:40:25 +0100

    ,

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
   const active = useActiveSection(["home", "services", "our-work", "contact-us"]);
 
   return (
-    <div className="flex justify-between items-center px-4 sm:px-2 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl bg-white font-medium border">
+    <div className="flex justify-between items-center px-4 sm:px-2 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl  font-medium ">
       {/* Logo */}
       <div className="flex items-center gap-1">
         <Image src="/volume.jpg" alt="VolumeUpDotCom Logo" width={50} height={50} />
