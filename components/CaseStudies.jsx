ce16349949956aaf281f51256b3cab86435c5bb3
Daniel Asena <asenadaniel@gmail.com>
2025-09-12 19:26:40 +0100

    .

--- components/CaseStudies.jsx
+++ components/CaseStudies.jsx
@@ -35,7 +35,7 @@ export default function CaseStudies() {
       <div className="container mx-auto px-4 text-center flex flex-col items-center gap-8 justify-center ">
         <h2 className="text-3xl font-bold mb-2">Case Studies</h2>
 
-        <div className="grid md:grid-cols-3 lg:grid-cols-5 gap-8 ">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8 ">
           {steps.map((step) => (
             <div
               key={step.id}
