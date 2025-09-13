de33584dade0a477efd69d17ebafeb8d0731a0c3
Daniel Asena <asenadaniel@gmail.com>
2025-09-13 04:02:36 +0100

    .

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -36,43 +36,78 @@ function Hero() {
       </motion.div>
 
       {/* Hero Title */}
-      <h1 className='max-w-3xl mx-auto'>
+      <motion.h1
+        initial={{ opacity: 0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 1 }}
+        viewport={{ once: true }}
+
+        className='max-w-3xl mx-auto'>
         <span className='font-extrabold'>Volume Up Dot Com Limited</span> is a Nigerian experiential
         <span className='font-semibold text-[#72120e]'> Marketing</span>,
         <span className='font-semibold text-[#72120e]'> Media</span> and
         <span className='font-semibold text-[#72120e]'> Logistics</span> consulting firm.
         We deploy strategic solutions that help brands extract value and build market leadership.
-      </h1>
+      </motion.h1>
 
       {/* CTA Button */}
-      <div
+      <motion.div
+
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 0.7 }}
+        viewport={{ once: true }}
 
         className="mt-4 inline-block bg-[#72120e] text-white px-8 py-3 rounded-full font-semibold shadow-lg hover:bg-[#bc383b] transition-transform"
       >
         Let’s Work Together
-      </div>
+      </motion.div>
 
       {/* Hero Image */}
-      <div className="mt-6">
+      <motion.div
+
+        initial={{ opacity: 0, scale: 0.9 }}
+        whileInView={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.6, delay: 1 }}
+        viewport={{ once: true }}
+
+        className="mt-6">
         <img src="/VolumeUPhero.jpg" alt="Hero Banner" className='max-w-6xl w-full' />
         <div className='relative'>
           <Image src="/volume.jpg" alt="VolumeUpDotCom Logo" width={50} height={50} className='absolute bottom-1' />
           <p className='absolute bottom-2 left-16 text-xs font-semibold text-[#72120e] italic'>VolumeUP</p>
         </div>
-      </div>
+      </motion.div>
 
       {/* Quotes */}
       <div className='flex flex-col items-center gap-2 mt-4'>
-        <p className='text-gray-500 italic w-72 md:w-96 lg:w-[600px]'>
+        <motion.p
+
+          initial={{ opacity: 0, y: 30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, delay: 0.7 }}
+          viewport={{ once: true }}
+
+          className='text-gray-500 italic w-72 md:w-96 lg:w-[600px]'>
           "A Brand is not so much about rational arguments, but the way the company resonates with the people emotionally.
           <span className='text-[#72120e] font-semibold'> MARKETING IS ABOUT VALUES</span>"
-        </p>
-        <p className='text-gray-500 italic w-72 md:w-96 lg:w-[600px]'>
+        </motion.p>
+
+        <motion.p
+          initial={{ opacity: 0, y: -20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, delay: 0.7 }}
+          viewport={{ once: true }}
+
+          className='text-gray-500 italic w-72 md:w-96 lg:w-[600px]'>
           "Creativity is intelligence having fun"
-        </p>
+        </motion.p>
       </div>
     </div>
   )
 }
 
 export default Hero
+
+
+
